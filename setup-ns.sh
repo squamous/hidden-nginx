fa5cd3aac28474ff135fe0a55e2a0461bc6799da
Sina Sadeghi <sina.sa@gmail.com>
2015-04-04 13:46:18 +1100

    fixup setup-ns.sh
    removes superfluous ip addr add line (lo already has addr) and
    adds ip netns exec nginx to set lo up

--- setup-ns.sh
+++ setup-ns.sh
@@ -3,8 +3,7 @@ ip link add veth0 type veth peer name veth1
 ip link set veth0 up
 ip addr add 192.168.254.253/24 broadcast 192.168.254.255 dev veth0
 ip netns add nginx
-ip link set lo up
-ip addr add 127.0.0.1/8 dev lo
+ip netns exec nginx ip link set lo up
 ip link set veth1 netns nginx
 ip netns exec nginx ip link set veth1 up
 ip netns exec nginx ip addr add 192.168.254.254/24 broadcast 192.168.254.255 dev veth1

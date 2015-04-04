4a1d650ea298d29525a1b701ae46a2e9cb1dd9a1
Sina Sadeghi <sina.sa@gmail.com>
2015-04-04 13:25:10 +1100

    add setup-php.sh
    script installs php5-fpm from deb repo, modifies init script
    so that fpm starts in nginx netns, runs in the nginx chroot,
    listens on a port rather than socket and fixes cgi_pathinfo.

---
+++ setup-php.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+apt-get -y install php5-fpm
+service php5-fpm stop
+sed -i 's/start-stop-daemon --start/ip netns exec nginx start-stop-daemon --start/' /etc/init.d/php5-fpm
+sed -i 's/^;chroot.*/chroot = \/home\/nginx/' /etc/php5/fpm/pool.d/www.conf
+sed -i 's/^listen = .*/listen = 9000/' /etc/php5/fpm/pool.d/www.conf
+sed -i 's/^;cgi.fix_pathinfo.*/cgi.fix_pathinfo = 0/' /etc/php5/fpm/pool.d/www.conf
+service php5-fpm start
